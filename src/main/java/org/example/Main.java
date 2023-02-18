9d5345909cb714f02fac4253b597efe6b458c3ea
Nikiforos Xylogiannopoulos <nikiforos.xylogiannopoulos@etraveligroup.com>
2023-02-18 20:45:14 +0200

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author Nikiforos Xylogiannopoulos
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
